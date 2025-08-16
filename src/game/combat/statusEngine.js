5131ac60e48e74ca2aeb16fd4a5990c26f8b94cf
recalledz <f.froilanrivera@gmail.com>
2025-08-16 18:24:01 -0400

    refactor status effects and add stun bar

---
+++ src/game/combat/statusEngine.js
@@ -0,0 +1,11 @@
+import { STATUSES } from '../../data/status.js';
+
+export function applyStatus(target, key, power, state) { // STATUS-REFORM
+  const def = STATUSES[key];
+  if (!def) return;
+  if (!target.statuses) target.statuses = {};
+  const current = target.statuses[key] || { stacks: 0, duration: 0 };
+  current.stacks = Math.min(def.maxStacks ?? Infinity, current.stacks + 1);
+  current.duration = def.duration;
+  target.statuses[key] = current;
+}
