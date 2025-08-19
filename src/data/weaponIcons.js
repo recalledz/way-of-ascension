ba88a15f033e6a8030f9350cc305db2239bcfd99
recalledz <f.froilanrivera@gmail.com>
2025-08-18 20:41:28 -0400

    chore: add weapon icon data

---
+++ src/data/weaponIcons.js
@@ -0,0 +1,12 @@
+export const WEAPON_ICONS = {
+  sword: 'ph:sword-light',
+  hammer: 'akar-icons:hammer',
+  spear: 'mdi:spear',
+  fist: 'game-icons:fist',
+  nunchaku: 'game-icons:nunchaku',
+  palm: 'ph:hand-palm-thin',
+  wand: 'fluent:wand-16-regular',
+  focus: 'ri:focus-line',
+  chakram: 'game-icons:chakram',
+  scepter: 'game-icons:winged-scepter',
+};
