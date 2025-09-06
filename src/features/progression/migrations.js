3c15ae29626705c8c63436a2520752478b83d770
recalledz <f.froilanrivera@gmail.com>
2025-09-06 10:01:57 -0400

    refactor: move law init to progression

--- src/features/progression/migrations.js
+++ src/features/progression/migrations.js
@@ -1,17 +1,26 @@
+import { checkLawUnlocks } from './mutators.js';
+
+export function ensureLaws(root){
+  if(!root.laws){
+    root.laws = {
+      selected: null,
+      unlocked: [],
+      points: 0,
+      trees: {
+        sword: {},
+        formation: {},
+        alchemy: {},
+      },
+    };
+  }
+  if(typeof checkLawUnlocks === 'function'){
+    checkLawUnlocks(root);
+  }
+}
+
 export const migrations = [
   save => {
-    if(!save.laws){
-      save.laws = {
-        selected: null,
-        unlocked: [],
-        points: 0,
-        trees: {
-          sword: {},
-          formation: {},
-          alchemy: {},
-        },
-      };
-    }
+    ensureLaws(save);
     if(!save.cultivation){
       save.cultivation = {
         talent: 1.0,
